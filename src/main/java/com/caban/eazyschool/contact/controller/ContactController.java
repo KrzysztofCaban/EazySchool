79bef573671edb8f0bc53fc272b5b21fa672dd13
Krzysztof Caban <kcaban99@gmail.com>
2024-02-25 17:38:17 +0100

    added contact page with model, controller and service

---
+++ src/main/java/com/caban/eazyschool/contact/controller/ContactController.java
@@ -0,0 +1,29 @@
+package com.caban.eazyschool.contact.controller;
+
+import com.caban.eazyschool.contact.model.Contact;
+import com.caban.eazyschool.contact.service.ContactService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    private final ContactService contactService;
+
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping("/contact")
+    public String displayContactPage() {
+        return "contact";
+    }
+
+    @PostMapping( "/saveMsg")
+    public String saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
+        return "redirect:/contact";
+    }
+}
